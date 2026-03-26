d3337aa16612c4c6aa921d3155fee55d455e4490
Phoenix <phoenix@2609.com>
2026-03-26 12:37:12 +0300

    feat:set proxy configure

--- src/setup/backend.setup.ts
+++ src/setup/backend.setup.ts
@@ -14,6 +14,7 @@ import { websocketSetup } from "./websocket.setup";
 
 export const backendSetup = () => {
   const app: Express = express();
+  app.set('trust proxy', 1);
   const limiter: RateLimitRequestHandler = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
